343d03629ddd8f6054f4ff0026be9fc2287863b0
mkarthik90 <mkarthik90@gmail.com>
2015-10-12 11:01:44 -0700

    Added a simple parser for parsing brackets.

---
+++ DynamicProgram/src/SimpleParser.java
@@ -0,0 +1,47 @@
+import java.util.Stack;
+
+public class SimpleParser {
+
+	public boolean parser(String arr) {
+		Stack<Character> stack = new Stack<Character>();
+
+		for (int i = 0; i < arr.length(); i++) {
+			Character character = arr.charAt(i);
+			if (character.equals('{') || character.equals('(')) {
+				stack.push(character);
+			}
+
+			else if (character.equals('}')) {
+				if (!(stack.pop().equals('{'))) {
+					return false;
+				}
+			}
+
+			else if (character.equals(')')) {
+				if (!(stack.pop().equals('('))) {
+					return false;
+				}
+			}
+
+			else {
+				return false;
+			}
+
+		}
+
+		return true;
+	}
+
+	public static void main(String[] args) {
+		// This parser method validates if the brackets have been opened and
+		// closed correctly
+		// For example {{}} ({}) {()} ({{{}}}) are all valid
+		// Invalid {(}) {{{))) {({)}
+
+		SimpleParser parser = new SimpleParser();
+		System.out.println(parser.parser("{{}}"));
+		System.out.println(parser.parser(""));
+		System.out.println(parser.parser("{{{)))"));
+	}
+
+}
