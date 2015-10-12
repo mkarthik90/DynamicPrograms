7311f21eaca37da74065af1145a5dac54ebf02a2
mkarthik90 <mkarthik90@gmail.com>
2015-10-11 22:45:50 -0700

    Issues for negative values fixed

--- DynamicProgram/src/SubArraySum.java
+++ DynamicProgram/src/SubArraySum.java
@@ -5,7 +5,9 @@ public class SubArraySum {
 		int currentMaxSum = 0;
 		for (int i = 0; i < arrayElements.length; i++) {
 			currentMaxSum = currentMaxSum + arrayElements[i];
-			if (currentMaxSum > finalMaxSum && currentMaxSum > 0) {
+			// Included for the first element alone. Irrespective of value of
+			// first element, it is the greatest in the array.
+			if (i == 0 | currentMaxSum > finalMaxSum) {
 				finalMaxSum = currentMaxSum;
 			}
 			if (currentMaxSum < 0) {
@@ -20,6 +22,14 @@ public class SubArraySum {
 		int arr[] = { -2, -3, 4, -1, -2, 1, 5, -3 };
 		SubArraySum subArraySum = new SubArraySum();
 		subArraySum.maxSubArray(arr);
+
+		// Testing for all negative numbers
+		int arr1[] = { -6, -3, -1, -5, -2 };
+		subArraySum.maxSubArray(arr1);
+
+		// Third test case
+		int arr2[] = { 100, -2, -3, 12, 23, 45, 34, 56, -1, -5, 200, -6 };
+		subArraySum.maxSubArray(arr2);
 	}
 
 }
