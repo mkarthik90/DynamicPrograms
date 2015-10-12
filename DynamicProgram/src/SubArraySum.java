1c24a0f5712ab257f084b2f2714db8365bdb61fa
mkarthik90 <mkarthik90@gmail.com>
2015-10-11 21:59:50 -0700

    Added first program for calculating Maximum continues sub array.

--- DynamicProgram/src/SubArraySum.java
+++ DynamicProgram/src/SubArraySum.java
@@ -1,23 +1,23 @@
 public class SubArraySum {
 
-	public int maxSubArray(int[] arrayElements){
+	public int maxSubArray(int[] arrayElements) {
 		int finalMaxSum = 0;
 		int currentMaxSum = 0;
-		for(int i=0;i<arrayElements.length;i++){
-				currentMaxSum = currentMaxSum + arrayElements[i];
-			if(currentMaxSum>finalMaxSum && currentMaxSum>0){
+		for (int i = 0; i < arrayElements.length; i++) {
+			currentMaxSum = currentMaxSum + arrayElements[i];
+			if (currentMaxSum > finalMaxSum && currentMaxSum > 0) {
 				finalMaxSum = currentMaxSum;
 			}
-			if(currentMaxSum<0){
+			if (currentMaxSum < 0) {
 				currentMaxSum = 0;
 			}
 		}
-		System.out.println("Maximum sum of Sub Array"+finalMaxSum);
+		System.out.println("Maximum sum of Sub Array" + finalMaxSum);
 		return 0;
 	}
-	
+
 	public static void main(String[] args) {
-		int arr[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+		int arr[] = { -2, -3, 4, -1, -2, 1, 5, -3 };
 		SubArraySum subArraySum = new SubArraySum();
 		subArraySum.maxSubArray(arr);
 	}
